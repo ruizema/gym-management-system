0c7d3efe4f9529408060b60d8095911f8812ad8f
Rui Ze Ma <mmrz33@gmail.com>
2019-06-03 00:57:21 -0400

    Reorganized folder structure & started prototype

---
+++ Implementation/Source/CentralServer.java
@@ -0,0 +1,2 @@
+public class CentralServer {
+}
---
+++ Implementation/Source/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    private CentralServer server;
+
+    public static void main(String[] args) {
+        /* Centre Sportif #GYM
+               ---- Choisissez une option ----
+               1. Création d'un compte
+               2. Création d'une séance
+               3. (...)
+        */
+    }
+}
