4f23a5e498f784844f16143f57d3d6a7adcc4043
Rui Ze Ma <mmrz33@gmail.com>
2019-06-03 16:01:21 -0400

    Added new classes & implemented template for main menu

--- Implementation/Source/Main.java
+++ Implementation/Source/Main.java
@@ -1,13 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
 
     private CentralServer server;
 
     public static void main(String[] args) {
-        /* Centre Sportif #GYM
-               ---- Choisissez une option ----
-               1. Création d'un compte
-               2. Création d'une séance
-               3. (...)
-        */
+        Scanner scanner = new Scanner(System.in);
+        Boolean exit = false;
+        do {
+            mainMenu();
+            switch (scanner.nextLine()) {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                // (...)
+                case "q":
+                    exit = true;
+            }
+        } while (!exit);
+    }
+
+    private static void mainMenu() {
+        System.out.println("Centre Sportif #GYM\n" +
+                "---- Choisissez une option ----\n" +
+                "1. Création d'un compte\n" +
+                "2. Création d'une séance\n" +
+                "3. (...)");
+        // TODO: add all menu options
     }
 }
