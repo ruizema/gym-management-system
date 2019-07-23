0a84f293e820ed10ffd93157d74d05d0cac05bec
Rana Nawfal <rana.nawfal@live.com>
2019-07-23 14:28:09 -0400

    validation des donnees

--- Implementation/Source/DataRecord.java
+++ Implementation/Source/DataRecord.java
@@ -1,4 +1,6 @@
 import java.util.HashMap;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.LinkedList;
 import java.util.Map;
 
@@ -13,12 +15,12 @@ public abstract class DataRecord {
                 "Date de début du service (JJ-MM-AAAA)",
                 "Date de fin du service (JJ-MM-AAAA)",
                 "Heure du service (HH:MM)",
-                "Récurrence hebdomadaire du service (quels jours il est offert à la même heure)",
+                "Récurrence hebdomadaire du service (D,L,Ma,Me,J,V,S) (quels jours il est offert à la même heure)",
                 "Capacité maximale (maximum 30 inscriptions)",
                 "Numéro du professionnel (9 chiffres)",
                 "Code du service (7 chiffres)",
                 "Code de la séance (7 chiffres)",
-                "Frais du service (en $, 5 chiffres dont 2 décimales)",
+                "Frais du service (xxx.xx) (en $, 5 chiffres dont 2 décimales)",
                 "Commentaires (100 caractères) (facultatif).",
             }, "Registration", new String[]{
                 "Date et heure actuelles (JJ-MM-AAAA HH:MM:SS)",
@@ -42,7 +44,7 @@ public abstract class DataRecord {
                 "Ville du client (14 caractères)",
                 "Province du client (2 lettres)",
                 "Code postal du client (6 caractères)",
-                "Status du client (Membre ou Professionnel)",
+                "Status du client (M ou P) (Membre ou Professionnel)",
             }));
     String[] data;
 
@@ -63,9 +65,8 @@ public abstract class DataRecord {
     }
     
     public boolean validation (String dataType , String field , int i) {
-    	String[] DataRecord.getFieldNames(dataType);
-    	// parse whats in parenthesis
-    	String[] parse = DataRecord.getFieldNames(dataType)[i].split("[()]+");
+    	// parse what is in parenthesis
+    	String[] parse = getFieldNames(dataType)[i].split("[()]+");
     	String[] restriction = parse[1].split("[ ]+");
     	
     	switch (restriction[1]) {
@@ -76,27 +77,111 @@ public abstract class DataRecord {
 	        		  } catch (NumberFormatException e) {
 	        		    return false;
 	        		  }
-	        	 // check if lenght is less than max
-	        	if ( String.valueOf(result).length()<= restriction[0] )
+	        	 // check if length is less than max
+	        	if ( field.length()<= Integer.parseInt(restriction[0]) )
 	        	{
 	        		return true;
 	        	}
-	            break;
+	        	return false;
+	           
 	        case "lettres":
 	        	if(field.matches("[a-zA-Z]+") == true) {
-	        		if(field.length() <= restriction[0]) {
+	        		if(field.length() <= Integer.parseInt(restriction[0])) {
 	        			return true;
 	        		}
 	        	}
 	        	return false;
-	        	break;
 	        case "caractères":
-	        	if(field.length() <= restriction[0]) {
+	        	if(field.length() <= Integer.parseInt(restriction[0])) {
 	        		return true;
 	        	}
 	        	return false;
-	        	break;
+	        	
+	        
+    	 }
+	        	
+    	switch (restriction[0]) {
+    		case "M":
+    			if(field == "M" || field == "P" ) {
+    				return true;
+    			}
+    			return false;
+    		case "JJ-MM-AAAA":
+    			if (restriction[1] == "HH:MM:SS" )
+    				return isValidDateTime(field);
+    			else
+    				return isValidDate(field);
 
+    			
+    		case "xxx.xx":
+    			try {
+	                Double num = Double.parseDouble(field);
+	            } catch (NumberFormatException e) {
+	                return false;
+	            }
+	        
+	        	String[] parts = field.split(".");
+	        	if (parts[0].length() == 3 && parts[1].length() == 2) {
+	        		return true;
+	        	}
+	        	else 
+	        		return false;
+    		case "HH:MM":
+    			return isValidTime(field);
+    			
+    		case "D" :
+    			String[] jours = field.split(",");
+    			if( jours.length > 7)
+    				return false;
+    		 
+    		case "maximum":
+    			try {
+        		 	int result = Integer.parseInt(field);
+        		  } catch (NumberFormatException e) {
+        		    return false;
+        		  }
+    			
+    			if ( Integer.parseInt(field) <= 30)
+    				return true;
+    				else 
+    					return false;
+    			
+    			
+    	}
     	
+    	return true;
+	
+    }
+    
+    public static boolean isValidDateTime(String inDate) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+        dateFormat.setLenient(false);
+        try {
+            dateFormat.parse(inDate.trim());
+        } catch (ParseException pe) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isValidDate(String inDate) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        dateFormat.setLenient(false);
+        try {
+            dateFormat.parse(inDate.trim());
+        } catch (ParseException pe) {
+            return false;
+        }
+        return true;
+    }
+    public static boolean isValidTime(String inDate) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm");
+        dateFormat.setLenient(false);
+        try {
+            dateFormat.parse(inDate.trim());
+        } catch (ParseException pe) {
+            return false;
+        }
+        return true;
     }
 }
