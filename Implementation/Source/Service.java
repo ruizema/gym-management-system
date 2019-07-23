78d13768a48a2e950984acff6e56272ba5344e3d
Jasmine <jasmine.pankham@umontreal.ca>
2019-07-23 06:48:20 -0400

    <add service.java>

---
+++ Implementation/Source/Service.java
@@ -0,0 +1,18 @@
+public class Service extends DataRecord {
+    private String name;
+    private String id;
+
+    public Service(String[] data) {
+        super(data);
+        this.name = data[0];
+        this.id = data[1];
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
