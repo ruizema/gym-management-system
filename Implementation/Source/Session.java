29962199c16abbd38ef97fadbf9ed560b6dfcbfb
ruizema <mmrz33@gmail.com>
2019-07-23 23:48:03 -0400

    Final commit for the day

--- Implementation/Source/Session.java
+++ Implementation/Source/Session.java
@@ -1,14 +1,16 @@
 public class Session extends DataRecord {
+    private String id;
+
     public Session(String[] data) {
         super(data);
+        id = data[7].substring(0, 3) + data[8].substring(0, 2) + data[6].substring(data[6].length() - 2);
     }
 
     public double getPrice() {
         return Double.parseDouble(data[9]);
     }
 
-    // TODO: generate sessionId & overwrite whatever the user entered lmao
     public String getSessionId() {
-        return data[8];
+        return id;
     }
 }
