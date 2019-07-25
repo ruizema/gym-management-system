87ade5da9b2da16968d03a114707a13fff288f01
Jasmine <jasmine.pankham@umontreal.ca>
2019-07-24 23:26:02 -0400

    tests done

--- Implementation/Source/DataCentreTest.java
+++ Implementation/Source/DataCentreTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.LinkedList;
 
 public class DataCentreTest {
     DataCentre dataCenter;
@@ -17,25 +18,31 @@ public class DataCentreTest {
     Account a2;
     Account a3;
 
-    Session b;
-    DataCentre c;
     Service d;
-
-    Registration r1;
-    Registration r2;
-
+    Session e;
 
     @Before
-    public void setUp() {
+    public void setUp() throws IOException{
+        //Accounts
         String[] dataA = {"ab@gmail.com","ZY","999 moland","Tuni","MO","5G5G5G","M"};
-        a1 = new Account(dataA);
-        a2 = new Account(new String[]{"mmrz33@gmail.com", "Rui Ze Ma", "5555 Rome", "Brossard", "QC", "J4K3R7", "M"});
-        a3 = new Account(new String[]{"abcde@gmail.com", "Bob Lol", "1234 Sherbrooke", "Montreal", "QC", "H4B2T6", "P"});
+        String[] dataB = {"mmrz33@gmail.com", "Rui Ze Ma", "5555 Rome", "Brossard", "QC", "J4K3R7", "M"};
+        String[] dataC = {"abcde@gmail.com", "Bob Lol", "1234 Sherbrooke", "Montreal", "QC", "H4B2T6", "P"};
+        dataCenter.createDataRecord(dataA, "Account");
+        dataCenter.createDataRecord(dataB, "Account");
+        dataCenter.createDataRecord(dataC, "Account");
+        LinkedList<Account> a = dataCenter.getDataRecords("Account");
+        a1 = a.get(0);
+        a2 = a.get(1);
+        a3 = a.get(2);
 
+        //Service
         String[] dataD = {"yoga"};
-        d = new Service(dataD);
+        dataCenter.createDataRecord(dataD, "Service");
+        LinkedList<Service> service = dataCenter.getDataRecords("Service");
+        d = service.get(0);
 
-        String[] dataB = {
+        //Session
+        String[] dataE = {
                 "20-07-2019 12:59:59",
                 "20-07-2019",
                 "20-08-2019",
@@ -47,50 +54,38 @@ public class DataCentreTest {
                 "6549872",
                 "135.45",
                 "Apporter vos tapis."};
+        dataCenter.createDataRecord(dataE, "Session");
+        LinkedList<Session> session = dataCenter.getDataRecords("Session");
+        e = session.get(0);
 
-        b = new Session(dataB);
-
-        String userId = a2.getId();
-        String serviceId = d.getId();
-        String sessionId = b.getSessionId();
-        String otherUserId = a3.getId();
-        r1 = new Registration(new String[]{"23-07-2019 19:54:23", "25-07-2019", a3.getId(), userId, serviceId, sessionId, ""});
-        r2 = new Registration(new String[]{"23-07-2019 19:54:23", "25-07-2019", a3.getId(), otherUserId, serviceId, sessionId, ""});
+        //Registrations
+        String[] dataF = {"23-07-2019 19:54:23", "25-07-2019", a3.getId(), a1.getId(), d.getId(), e.getSessionId(), ""};
+        String[] dataG = {"23-07-2019 19:54:23", "25-07-2019", a3.getId(), a2.getId(), d.getId(), e.getSessionId(), ""};
+        dataCenter.createDataRecord(dataF, "Registration");
+        dataCenter.createDataRecord(dataG, "Registration");
     }
 
-/*********TODO*********/
     @Test
     public void testLogin() {
         String email = a1.getEmail();
-        String[] login =  c.login(email);
+        String[] login =  dataCenter.login(email);
+
         assertEquals("0", login);
     }
-    
 
     @Test
-    public void testValidateId() {
-    	 a1.generateId();
-         String id = a1.getId();
-         
-         int code = 8;
-         
-         try {
-			 code =  c.validateId(id);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			
-		}
+    public void testValidateId() throws IOException{
+        String id = a1.getId();
+        int result = dataCenter.validateId(id);
 
-         assertEquals(0, code);
-
+        assertEquals(0, result);
     }
 
-    
     @Test
     public void testValidatePresence() throws IOException {
         String userId = a2.getId();
         String serviceId = d.getId();
-        String sessionId = b.getSessionId();
+        String sessionId = e.getSessionId();
         String otherUserId = a3.getId();
         assertTrue(dataCenter.validatePresence(userId, serviceId, sessionId) && dataCenter.validatePresence(otherUserId, serviceId, sessionId));
     }
